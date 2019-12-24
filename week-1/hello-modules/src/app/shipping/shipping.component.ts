a165d204a8f1474ac379d2fc0f8d0c4b92236759
Loren Wetzel <l.wetzel900@gmail.com>
2019-12-23 18:16:05 -0600

    adding week 1 assignmet 1.5

---
+++ week-1/hello-modules/src/app/shipping/shipping.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping',
+  template: `
+    <p>
+      Shipping Component works?
+    </p>
+  `,
+  styles: []
+})
+export class ShippingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
